2b1bec23f22720d53d8a83438a7b7d4e3221e3a0
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2020-08-10 22:09:07 -0300

    Isolando Componentes 02:41

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button: React.FC = () => (
+  <button type="button">teste</button>
+);
+
+export default Button;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Input: React.FC = () => (
+  <input type="text" />
+);
+
+export default Input;
