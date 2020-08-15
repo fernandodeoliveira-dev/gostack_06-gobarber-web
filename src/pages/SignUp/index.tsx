959144be6ef05942082e844a225b095a22728e08
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2020-08-15 02:33:00 -0300

    Utilizando Unform - done

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
+const SignUp: React.FC = () => {
+
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
     <Background />
     <Content>
 
       <img src={logoImg} alt="GoBarber" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <h1>Faca seu cadastro</h1>
 
         <Input
@@ -33,7 +40,7 @@ const SignUp: React.FC = () => (
 
         <Button type="submit">Cadastrar</Button>
 
-      </form>
+      </Form>
 
       <a href="login">
         <FiArrowLeft />
@@ -42,6 +49,7 @@ const SignUp: React.FC = () => (
 
     </Content>
   </Container>
-);
+  );
+};
 
 export default SignUp;
