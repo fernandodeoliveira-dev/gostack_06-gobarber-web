b7b8ba38ed1030144af56b2feca9fd4062076e81
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2020-08-18 23:32:58 -0300

    Criando tooltip de erros - done

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -3,11 +3,12 @@ import { Container } from './styles';
 
 interface TooltipProps {
   title: string;
+  className?: string;
 }
 
-const Tooltip: React.FC<TooltipProps> = ({ title, children }) => {
+const Tooltip: React.FC<TooltipProps> = ({ title, className = '', children }) => {
   return (
-    <Container>
+    <Container className={className}>
       {children}
       <span>{title}</span>
     </Container>
