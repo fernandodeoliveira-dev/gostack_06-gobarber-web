b044d72caf5696eae4988cea0fb0757df8f36a16
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2020-08-25 23:57:03 -0300

    FIX

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -29,12 +29,12 @@ const ToastProvider: React.FC = ({ children }) => {
       description
     }
 
-    setMessages((state) => [ ... state, toast]);
+    setMessages((state) => [ ...state, toast]);
 
   }, []);
 
   const removeToast = useCallback((id: string) => {
-    setMessages((state) => state.filter(message => message.id != id));
+    setMessages((state) => state.filter(message => message.id !== id));
   }, []);
 
   return (
